c626da5c96bba627bdf3a885d3c663b7c9d20077
Mark Larah <markl@yelp.com>
2021-03-22 15:10:18 -0700

    fix filesystem examples

--- src/filesystem/__mocks__/fs.js
+++ src/filesystem/__mocks__/fs.js
@@ -1,13 +1,10 @@
 import { Volume } from 'memfs';
-import { Union } from 'unionfs';
 
-const ufs = new Union();
 const vol = new Volume();
-ufs.use(vol);
 
 export function __setVolumeContents(volumeJson) {
     vol.reset();
     vol.fromJSON(volumeJson);
 }
 
-export default ufs;
+export default vol;
