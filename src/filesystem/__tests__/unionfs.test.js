6409e7a177fad62bb0d4a3d31cecc485d5a8ac16
Mark Larah <mark@larah.me>
2025-08-18 12:31:46 -0500

    example

--- src/filesystem/__tests__/unionfs.test.js
+++ src/filesystem/__tests__/unionfs.test.js
@@ -6,19 +6,27 @@ import path from 'path';
 jest.mock('fs', () => require('unionfs').ufs);
 jest.mock('fs/promises', () => require('unionfs').ufs.promises);
 
+const ROOT = path.join(__dirname, '..', '..', '..');
+
 vol.fromJSON({
-    '/foo/bar/baz.txt': 'hello from mock',
+    [path.join(ROOT, 'README.md')]: 'hi from mocked fs',
+    '/foo/bar/baz.txt': 'hi from mocked fs',
 });
 
 ufs.use(jest.requireActual('fs')).use(vol);
 
 it('unionfs', async () => {
+    // mocked files work
     const mockedPath = '/foo/bar/baz.txt';
-    expect(fs.readFileSync(mockedPath, 'utf8')).toBe('hello from mock');
-    await expect(fs.promises.readFile(mockedPath, 'utf8')).resolves.toBe('hello from mock');
+    expect(fs.readFileSync(mockedPath, 'utf8')).toBe('hi from mocked fs');
+    await expect(fs.promises.readFile(mockedPath, 'utf8')).resolves.toBe('hi from mocked fs');
 
-    const root = path.join(__dirname, '..', '..', '..');
-    const realPath = path.join(root, 'package.json');
+    // real files work
+    const realPath = path.join(ROOT, 'package.json');
     expect(fs.readFileSync(realPath, 'utf8')).toContain('jest-how-do-i-mock-x');
     await expect(fs.promises.readFile(realPath, 'utf8')).resolves.toContain('jest-how-do-i-mock-x');
+
+    // also we can override existing files with mocked files
+    const realPath2 = path.join(ROOT, 'README.md');
+    expect(fs.readFileSync(realPath2, 'utf8')).toBe('hi from mocked fs');
 });
